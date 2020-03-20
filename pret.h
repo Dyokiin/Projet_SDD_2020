beb519e6001a3a83edb33cbd0a4c6525729c4479
francois456 <55241946+francois456@users.noreply.github.com>
2020-03-20 14:33:04 +0100

    Update pret.h

--- pret.h
+++ pret.h
@@ -2,12 +2,12 @@
 
 typedef struct s_objet *Objet;
 enum type {livre, voiture, pc, outils};
-enregister_un_objet(char* nom,enum type);
-supprimer_un_objet(char* nom,enum type);
-emprunter(char* uti1,char* ut2,Objet objet);
-rendre(Objet objet);
-voir_historique(char *utilisateur);
-recherche(enum type);
+int enregister_un_objet(char* nom,enum type);
+int supprimer_un_objet(char* nom,enum type);
+int emprunter(char* uti1,char* ut2,Objet objet);
+int rendre(Objet objet);
+char* voir_historique(char *utilisateur);
+char* recherche(enum type);
 
-ecrire_json(char *texte);
-lire_json();
+int ecrire_json(char *texte);
+int lire_json();
