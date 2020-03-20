18ddd96b29357bacbd6041f5fe505b54a6e416f6
Franz <francois.marcelon@univ-tlse3.fr>
2020-03-20 14:26:13 +0100

    ven .h

---
+++ pret.h
@@ -0,0 +1,13 @@
+//pret echange
+
+typedef struct s_objet *Objet;
+enum type {livre, voiture, pc, outils};
+enregister_un_objet(char* nom,enum type);
+supprimer_un_objet(char* nom,enum type);
+emprunter(char* uti1,char* ut2,Objet objet);
+rendre(Objet objet);
+voir_historique(char *utilisateur);
+recherche(enum type);
+
+ecrire_json();
+lire_json();
