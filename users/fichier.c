850f136120f8e9729e387f77ba2eba0bcfc1f1d5
Franz <francois.marcelon@univ-tlse3.fr>
2020-03-20 13:58:15 +0100

    ven 13H58

--- users/fichier.c
+++ users/fichier.c
@@ -1,20 +1,61 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+
+int ecriture(char login[100],char password[100]){
+    /*char ligne_a_ajouter[200]="";
+    strcat(ligne_a_ajouter,login);
+    strcat(ligne_a_ajouter,"    ");
+    strcat(ligne_a_ajouter,password);
+    strcat(ligne_a_ajouter,"\n");*/
+    //printf("%s",ligne_a_ajouter );
     FILE *fichier=NULL;
-    fichier=fopen("./save.txt", "a");
+    fichier=fopen("./save.txt", "w");
     if (fichier!= NULL){
         printf("YES\n");
-        fputs("5678908765467897654356789876543\n",fichier);
+        fseek(fichier,0, SEEK_END);
+        //fputs(ligne_a_ajouter,fichier);
+        fprintf(fichier,"%s    %s\n",login,password);
         fclose(fichier);
+        return 1;
     }
     else{
-        printf("NO\n");
+        printf("Ecriture impossible : pas acces fichier\n");
+        return 0;
     }
-    fichier=fopen("./save.txt", "r");
+}
+
+
+
+int main(int argc, char *argv[]) {
+    char login[100]="azertyui";
+    char password[100]="aqzsedrf";
+    ecriture(login,password);
+
+
+    //char test[]="c:\\test.txt";
+    FILE *Fichier;
+    char motFr[100];
+    char motR[30]="coucou";
+    Fichier = fopen("./save.txt", "r");
+    if (!Fichier)
+         printf("ERREUR: Impossible d'ouvrir le fichier: ./save.txt.\n");
+
+
+    while (fgets(motFr,100,Fichier) != NULL)
+    {
+        if (strstr(motFr, motR) != NULL){
+            printf("'%s' trouvé dans '%s'\n", motR, motFr);
+        }
+    }
+    fclose(Fichier);
+
+
+
+    /*fichier=fopen("./save.txt", "r");
     char ligne[255];
     fgets(ligne, 200,fichier);
     printf("%s\n",ligne );
-    fclose(fichier);
+    fclose(fichier);*/
     return 0;
 }
