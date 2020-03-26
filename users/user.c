30b5f02dc13bcefcf751373b42786c762cf3dec3
francois456 <francois.marcelon@univ-tlse3.fr>
2020-03-26 12:50:43 +0100

    ok

--- users/user.c
+++ users/user.c
@@ -30,16 +30,22 @@ int new_user(){
         perror("malloc");
         exit(EXIT_FAILURE);
     }
-    printf("Votre nom d'utilisateur doit etre de taille min 4 et max %d. Ne pas contenir : d'espace,tab,é,ç,à,etc.\nEntrez nom d'utilisateur :",LONGEUR-2);
+    printf("Votre nom d'utilisateur doit etre de taille min 4 et max %d. Ne pas contenir : d'espace,tab,é,ç,à,etc. Saisissez 'q' pour retourner au menu\nEntrez nom d'utilisateur :",LONGEUR-2);
     do {
         while(!lire(u->id)){
             printf("Entrez nom d'utilisateur :");
         }
+        if (*(u->id)=='q'){
+            return -1;
+        }
     } while(recherche_occ(u->id) && printf("Identifiant deja existant. Entrez un autre nom d'utilisateur :"));
 
-    printf("Votre mot de passe doit etre de taille min 4 et max %d. Ne pas contenir : é,ç,à,etc.\nEntrez mot de passe :", LONGEUR-2);
+    printf("Votre mot de passe doit etre de taille min 4 et max %d. Ne pas contenir : é,ç,à,etc. Saisissez 'q' pour retourner au menu\nEntrez mot de passe :", LONGEUR-2);
     while(!lire(u->password)){
         printf("Entrez mot de passe :");
+    }
+    if (*(u->password)=='q'){
+        return -1;
     }
     u->s=BASIC;
     save_user(u);
@@ -54,6 +60,9 @@ int new_user(){
 int lire (char *chaine) {
     char *pointeur=NULL;
     if (fgets(chaine,LONGEUR,stdin) != NULL) {
+        if (strlen(chaine)==2 && chaine[0]=='q'){
+            return 1;
+        }
         if (strlen(chaine)<5){
             printf("Erreur : taille trop petite. ");
             return 0;
@@ -91,9 +100,12 @@ int save_user(User u){
 int connexion(){
     char Identifiant[LONGEUR];
     do { //mettre un exit
-        printf("Entrez nom d'utilisateur :");
+        printf("Saisissez 'q' pour retourner au menu. Entrez nom d'utilisateur :");
         while(!lire(Identifiant)){
             printf("Entrez nom d'utilisateur :");
+        }
+        if(*Identifiant=='q'){
+            return -1;
         }
     } while(!recherche_occ(Identifiant) && printf("Identifiant inexistant. "));
     char Mdp[LONGEUR];
@@ -103,6 +115,9 @@ int connexion(){
         printf("Entrez mot de passe :");
         while(!lire(Mdp)){
             printf("Entrez mot de passe :");
+        }
+        if(*Mdp=='q'){
+            return -1;
         }
         chiffrement(Mdp,Mdp_chiffre);
         recherche(Identifiant,ligne);
@@ -147,12 +162,19 @@ int menu_connexion(){
     }
     clear();
     if(chaine[0]=='1'){
-        printf("Connexion\n" );
-        connexion();
+        printf("Connexion Saisissez 'q' pour retourner au menu. \n" );
+        int a=connexion();
+        if (a==-1){
+            menu_connexion();
+        }
+
     }
     if(chaine[0]=='2'){
         printf("Creation d'un compte\n");
-        new_user();
+        int b=new_user();
+        if (b==-1){
+            menu_connexion();
+        }
     }
     return 1;
 }
--- users/user.h
+++ users/user.h
@@ -1,5 +1,5 @@
-#ifndef __USERS_H__
-#define __USERS_H__
+#ifndef __USER_H__
+#define __USER_H__
 
 typedef struct s_user *User;
 
