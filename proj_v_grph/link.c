9de676f43aafc2dd44773e5ef5cb75ff52482bbe
Dyokiin <theo.arnauld-des-lions@univ-tlse3.fr>
2020-04-15 22:36:20 +0200

    correction

---
+++ proj_v_grph/link.c
@@ -0,0 +1,10 @@
+#include "gestion_graphique.c"
+#include <stdio.h>
+#include "gestion_utilisateurs.h"
+#include "gestion_ressources.h"
+#include "lecture.h"
+
+
+int test_connexion(char *login, char *password){
+	if(strcomp(login, "test")==0 && strcomp(password, "test") == 0){
+		retur
