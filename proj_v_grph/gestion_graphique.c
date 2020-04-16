e173a3a33bfef06663d8d873206c0023f715dc9b
Dyokiin <theo.arnauld-des-lions@univ-tlse3.fr>
2020-04-16 15:00:50 +0200

    menu recherche / menu principal

--- proj_v_grph/gestion_graphique.c
+++ proj_v_grph/gestion_graphique.c
@@ -7,8 +7,99 @@
 
 static GtkWidget *pFenetre  ;
 static GtkWidget *pConnexion;
-static GtkWidget *pCreation ;
 static GtkWidget *pPrincipal;
+static GtkWidget *pRecherche;
+
+
+/* Retourne au menu Acceuil en détruisant les autres */
+void retour_menu_acceuil(){
+	gtk_widget_destroy(pConnexion);
+	pConnexion = NULL;
+	gtk_widget_destroy(pPrincipal);
+	gtk_widget_destroy(pRecherche);
+}
+
+/* Envoie vers le menu connexion avec un argument quelconque */
+void go_menu_connexion(){
+	if (pConnexion == NULL){
+		menu_connexion("Connexion");
+	}
+}
+
+/* Envoie vers le menu creation avec un argument quelconque */
+void go_menu_creation(){
+	if (pConnexion == NULL){
+		menu_creation("Creation de compte");
+	}
+}
+
+void go_menu_principal_user(){
+	gtk_widget_destroy(pRecherche);
+	pRecherche = NULL;
+	menu_principal_user();
+}
+
+
+/* permet l'affichage du menu d'acceuil */
+void menu_acceuil(){
+
+	/* Initialisation des différents pointeurs vers les objets présents dans la fenetre */
+	GtkWidget *pBouton[3];
+	GtkWidget *pLabel;
+	GtkWidget *pBoxV ;
+	GtkWidget *pBoxH ;
+	GtkWidget *pLogo ;
+
+	gchar *sUtf8 ;
+
+
+	/* Creation et initialisation de la fenetre */	
+	pFenetre = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(pFenetre), "EasyShare");
+	gtk_window_set_default_size(GTK_WINDOW(pFenetre), 400, 200);
+	gtk_window_set_position(GTK_WINDOW(pFenetre), GTK_WIN_POS_CENTER);
+	g_signal_connect(G_OBJECT(pFenetre), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	
+	/* Creation box verticale */
+	pBoxV = gtk_vbox_new(FALSE, 0);
+	gtk_container_add(GTK_CONTAINER(pFenetre), pBoxV);
+
+
+	/* Insertion du logo EasyShare */
+	pLogo = gtk_image_new_from_file("./logo_eshv1.png");
+	gtk_box_pack_start(GTK_BOX(pBoxV), pLogo, FALSE, FALSE, 5);
+
+	/* Creation, initialisation et insertion du Label EasyShare police Sans, Gras */
+	pLabel = gtk_label_new(NULL);
+	sUtf8 = g_locale_to_utf8("<span face=\"Sans\"><b>| EasyShare |</b></span>\n", -1, NULL, NULL, NULL) ;
+	gtk_label_set_markup(GTK_LABEL(pLabel), sUtf8) ;
+	g_free(sUtf8);
+	gtk_box_pack_start(GTK_BOX(pBoxV), pLabel, FALSE, FALSE, 5);
+
+	/* Creation et insertion box horizontale */
+	pBoxH = gtk_hbox_new(TRUE, 0);
+	gtk_box_pack_start(GTK_BOX(pBoxV), pBoxH, FALSE, FALSE, 5);
+	
+	/* Creation et placement des boutons avec du texte */
+	pBouton[0] = gtk_button_new_with_label("Quitter");
+	pBouton[1] = gtk_button_new_with_label("Se connecter");
+	pBouton[2] = gtk_button_new_with_label("Creer un compte");
+
+	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[1], TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[2], TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[0], TRUE, FALSE, 0);
+
+	/* Association des signaux à leur bouton respectif */
+	g_signal_connect(G_OBJECT(pBouton[0]), "clicked", G_CALLBACK(gtk_main_quit), NULL) ;
+	g_signal_connect(G_OBJECT(pBouton[1]), "clicked", G_CALLBACK(go_menu_connexion), NULL);
+	g_signal_connect(G_OBJECT(pBouton[2]), "clicked", G_CALLBACK(go_menu_creation), NULL) ;
+ 
+	/* Affichage de la fenetre acceuil et de tout ses widgets*/
+	gtk_widget_show_all(pFenetre);
+	
+
+
+}
 
 
 /* Permet l'affichage du menu d'acceuil */
@@ -64,11 +155,10 @@ void menu_connexion(char *info){
 	g_signal_connect(G_OBJECT(pBouton[0]), "clicked", G_CALLBACK(on_valider_connexion), (GtkWidget *) pBoxV);
 
 	gtk_widget_show_all(pConnexion);
-
-
 }
 
-/* A l'appuis du bouton valider dans menu connexion et creation compte, permet l'enregistrement de la saisie clavier presente dans les entry */
+
+/* A l'appuis du bouton valider dans menu connexion, permet l'enregistrement de la saisie clavier presente dans les entry */
 void on_valider_connexion(GtkWidget *pButton, gpointer data){
 
 	/* initialisation entrys temporaires / liste chainee / char * et malloc */
@@ -117,7 +207,64 @@ void on_valider_connexion(GtkWidget *pButton, gpointer data){
 	
 }
 
-/* A l'appuis du bouton valider dans menu connexion et creation compte, permet l'enregistrement de la saisie clavier presente dans les entry */
+/* Permet l'affichage du menu de creation de compte */
+void menu_creation(char* info){
+
+	/* Initialisation des différents pointeurs vers les objets présents dans la fenetre */
+	GtkWidget *pBouton[2];
+	GtkWidget *pLabel;
+	GtkWidget *pEntryNom ;
+	GtkWidget *pEntryMdp ;
+	GtkWidget *pBoxV;
+	GtkWidget *pBoxH;
+
+	/* Initialisation et creation de la fenetre */
+	pConnexion = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(pConnexion), "EasyShare : connexion");
+	gtk_window_set_default_size(GTK_WINDOW(pConnexion), 400, 200);
+	g_signal_connect(G_OBJECT(pConnexion), "destroy", G_CALLBACK(retour_menu_acceuil), NULL);
+
+	/* Initialisation du Label message */
+	pLabel = gtk_label_new(info);
+
+	/* Initialisation des deux boutons */
+	pBouton[0] = gtk_button_new_with_label("Valider");
+	pBouton[1] = gtk_button_new_with_label("Annuler");
+
+	/* initialisation des deux box*/
+	pBoxV = gtk_vbox_new(TRUE, 0);
+	pBoxH = gtk_hbox_new(TRUE, 0);
+	gtk_container_add(GTK_CONTAINER(pConnexion), pBoxV);
+
+	
+	/* initialisation et parametrage des deux entry */
+	pEntryNom = gtk_entry_new_with_max_length(22);
+	pEntryMdp = gtk_entry_new_with_max_length(22);
+
+	gtk_entry_set_text(GTK_ENTRY(pEntryNom), "Nom");
+	gtk_entry_set_text(GTK_ENTRY(pEntryMdp), "Motdepasse");
+
+	gtk_entry_set_visibility(GTK_ENTRY(pEntryMdp), FALSE);
+	gtk_entry_set_invisible_char(GTK_ENTRY(pEntryMdp), '*');
+
+	/* Construction de la fenetre */
+	gtk_box_pack_start(GTK_BOX(pBoxV), pLabel, TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(pBoxV), pEntryNom, FALSE, TRUE, 0) ;
+	gtk_box_pack_start(GTK_BOX(pBoxV), pEntryMdp, FALSE, TRUE, 0) ;
+	gtk_box_pack_start(GTK_BOX(pBoxV), pBoxH, TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[0], TRUE, FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[1], TRUE, FALSE, 0);
+
+
+	/* Connexion signaux boutons */
+	g_signal_connect(G_OBJECT(pBouton[1]), "clicked", retour_menu_acceuil, NULL);
+	g_signal_connect(G_OBJECT(pBouton[0]), "clicked", G_CALLBACK(on_valider_creation), (GtkWidget *) pBoxV);
+
+	gtk_widget_show_all(pConnexion);
+
+}
+
+/* A l'appuis du bouton valider dans creation compte, permet l'enregistrement de la saisie clavier presente dans les entry */
 void on_valider_creation(GtkWidget *pButton, gpointer data){
 
 	/* initialisation entrys temporaires / liste chainee / char * et malloc */
@@ -166,79 +313,7 @@ void on_valider_creation(GtkWidget *pButton, gpointer data){
 	
 }
 
-/* Permet l'affichage du menu de creation de compte */
-void menu_creation(char* info){
 
-	/* Initialisation des différents pointeurs vers les objets présents dans la fenetre */
-	GtkWidget *pBouton[2];
-	GtkWidget *pLabel;
-	GtkWidget *pEntryNom ;
-	GtkWidget *pEntryMdp ;
-	GtkWidget *pBoxV;
-	GtkWidget *pBoxH;
-
-	/* Initialisation et creation de la fenetre */
-	pCreation = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(pCreation), "EasyShare : connexion");
-	gtk_window_set_default_size(GTK_WINDOW(pCreation), 400, 200);
-	g_signal_connect(G_OBJECT(pCreation), "destroy", G_CALLBACK(retour_menu_acceuil), NULL);
-
-	/* Initialisation du Label message */
-	pLabel = gtk_label_new(info);
-
-	/* Initialisation des deux boutons */
-	pBouton[0] = gtk_button_new_with_label("Valider");
-	pBouton[1] = gtk_button_new_with_label("Annuler");
-
-	/* initialisation des deux box*/
-	pBoxV = gtk_vbox_new(TRUE, 0);
-	pBoxH = gtk_hbox_new(TRUE, 0);
-	gtk_container_add(GTK_CONTAINER(pCreation), pBoxV);
-
-	
-	/* initialisation et parametrage des deux entry */
-	pEntryNom = gtk_entry_new_with_max_length(22);
-	pEntryMdp = gtk_entry_new_with_max_length(22);
-
-	gtk_entry_set_text(GTK_ENTRY(pEntryNom), "Nom");
-	gtk_entry_set_text(GTK_ENTRY(pEntryMdp), "Motdepasse");
-
-	gtk_entry_set_visibility(GTK_ENTRY(pEntryMdp), FALSE);
-	gtk_entry_set_invisible_char(GTK_ENTRY(pEntryMdp), '*');
-
-	/* Construction de la fenetre */
-	gtk_box_pack_start(GTK_BOX(pBoxV), pLabel, TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(pBoxV), pEntryNom, FALSE, TRUE, 0) ;
-	gtk_box_pack_start(GTK_BOX(pBoxV), pEntryMdp, FALSE, TRUE, 0) ;
-	gtk_box_pack_start(GTK_BOX(pBoxV), pBoxH, TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[0], TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[1], TRUE, FALSE, 0);
-
-
-	/* Connexion signaux boutons */
-	g_signal_connect(G_OBJECT(pBouton[1]), "clicked", retour_menu_acceuil, NULL);
-	g_signal_connect(G_OBJECT(pBouton[0]), "clicked", G_CALLBACK(on_valider_connexion), (GtkWidget *) pBoxV);
-
-	gtk_widget_show_all(pCreation);
-
-}
-
-/* Retourne au menu Acceuil en détruisant les autres */
-void retour_menu_acceuil(){
-	gtk_widget_destroy(pConnexion);
-	gtk_widget_destroy(pCreation);
-	gtk_widget_destroy(pPrincipal);
-}
-
-/* Envoie vers le menu connexion avec un argument quelconque */
-void go_menu_connexion(){
-	menu_connexion("Connexion");
-}
-
-/* Envoie vers le menu creation avec un argument quelconque */
-void go_menu_creation(){
-	menu_creation("Creation de compte");
-}
 
 
 /* Permet l'affichage du menu principal Adminnistrateur*/
@@ -344,7 +419,7 @@ void menu_principal_user(){
 	gtk_box_pack_start(GTK_BOX(pBoxH[1]), pBouton[3], FALSE, TRUE, 0);
 
 	/* Association des boutons a leur signaux */
-//	g_signal_connect(GTK_BUTTON(pBouton[0]), "clicked", G_CALLBACK(), NULL);
+	g_signal_connect(GTK_BUTTON(pBouton[0]), "clicked", G_CALLBACK(menu_recherche), NULL);
 //	g_signal_connect(GTK_BUTTON(pBouton[1]), "clicked", G_CALLBACK(), NULL);
 //	g_signal_connect(GTK_BUTTON(pBouton[2]), "clicked", G_CALLBACK(), NULL);
 	g_signal_connect(GTK_BUTTON(pBouton[3]), "clicked", G_CALLBACK(retour_menu_acceuil), NULL);
@@ -354,64 +429,51 @@ void menu_principal_user(){
 }
 
 
-/* permet l'affichage du menu d'acceuil */
-void menu_acceuil(){
-
-	/* Initialisation des différents pointeurs vers les objets présents dans la fenetre */
-	GtkWidget *pBouton[3];
-	GtkWidget *pLabel;
-	GtkWidget *pBoxV ;
-	GtkWidget *pBoxH ;
-	GtkWidget *pLogo ;
-
-	gchar *sUtf8 ;
-
-
-	/* Creation et initialisation de la fenetre */	
-	pFenetre = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(pFenetre), "EasyShare");
-	gtk_window_set_default_size(GTK_WINDOW(pFenetre), 400, 200);
-	gtk_window_set_position(GTK_WINDOW(pFenetre), GTK_WIN_POS_CENTER);
-	g_signal_connect(G_OBJECT(pFenetre), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+/* Permet la recherche d'objet par type, pour les utilisateurs */
+void menu_recherche(){
+	if(pRecherche == NULL){
+		GtkWidget *pLabel[2];
+		GtkWidget *pBoxV;
+		GtkWidget *pBouton[6];
 	
-	/* Creation box verticale */
-	pBoxV = gtk_vbox_new(FALSE, 0);
-	gtk_container_add(GTK_CONTAINER(pFenetre), pBoxV);
-
-
-	/* Insertion du logo EasyShare */
-	pLogo = gtk_image_new_from_file("./logo_eshv1.png");
-	gtk_box_pack_start(GTK_BOX(pBoxV), pLogo, FALSE, FALSE, 5);
-
-	/* Creation, initialisation et insertion du Label EasyShare police Sans, Gras */
-	pLabel = gtk_label_new(NULL);
-	sUtf8 = g_locale_to_utf8("<span face=\"Sans\"><b>| EasyShare |</b></span>\n", -1, NULL, NULL, NULL) ;
-	gtk_label_set_markup(GTK_LABEL(pLabel), sUtf8) ;
-	g_free(sUtf8);
-	gtk_box_pack_start(GTK_BOX(pBoxV), pLabel, FALSE, FALSE, 5);
-
-	/* Creation et insertion box horizontale */
-	pBoxH = gtk_hbox_new(TRUE, 0);
-	gtk_box_pack_start(GTK_BOX(pBoxV), pBoxH, FALSE, FALSE, 5);
 	
-	/* Creation et placement des boutons avec du texte */
-	pBouton[0] = gtk_button_new_with_label("Quitter");
-	pBouton[1] = gtk_button_new_with_label("Se connecter");
-	pBouton[2] = gtk_button_new_with_label("Creer un compte");
-
-	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[1], TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[2], TRUE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(pBoxH), pBouton[0], TRUE, FALSE, 0);
-
-	/* Association des signaux à leur bouton respectif */
-	g_signal_connect(G_OBJECT(pBouton[0]), "clicked", G_CALLBACK(gtk_main_quit), NULL) ;
-	g_signal_connect(G_OBJECT(pBouton[1]), "clicked", G_CALLBACK(go_menu_connexion), NULL);
-	g_signal_connect(G_OBJECT(pBouton[2]), "clicked", G_CALLBACK(go_menu_creation), NULL) ;
- 
-	/* Affichage de la fenetre acceuil et de tout ses widgets*/
-	gtk_widget_show_all(pFenetre);
+		pRecherche = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+		gtk_window_set_title(GTK_WINDOW(pRecherche), "EasyShare : rechercher un Objet");
+		gtk_window_set_position(GTK_WINDOW(pPrincipal), GTK_WIN_POS_CENTER);
+		g_signal_connect(G_OBJECT(pRecherche), "destroy", G_CALLBACK(go_menu_principal_user), NULL);
 	
-
-
+		pBoxV = gtk_vbox_new(TRUE, 5);
+		gtk_container_add(GTK_CONTAINER(pRecherche), pBoxV);
+	
+		pLabel[0] = gtk_label_new("Vous souhaitez emprunter :");
+		pLabel[1] = gtk_label_new("______________");
+	
+		pBouton[0] = gtk_button_new_with_label("Vehicule");
+		pBouton[1] = gtk_button_new_with_label("Livre");
+		pBouton[2] = gtk_button_new_with_label("DVDs");
+		pBouton[3] = gtk_button_new_with_label("Plante");
+		pBouton[4] = gtk_button_new_with_label("Outils");
+		pBouton[5] = gtk_button_new_with_label("Annuler");
+	
+		gtk_box_pack_start(GTK_BOX(pBoxV), pLabel[0], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pBouton[0], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pBouton[1], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pBouton[2], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pBouton[3], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pBouton[4], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pLabel[1], FALSE, TRUE, 5);
+		gtk_box_pack_start(GTK_BOX(pBoxV), pBouton[5], FALSE, TRUE, 5);
+	
+//		g_signal_connect(GTK_BUTTON(pBouton[0]), "clicked", G_CALLBACK(), NULL);
+//		g_signal_connect(GTK_BUTTON(pBouton[1]), "clicked", G_CALLBACK(), NULL);
+//		g_signal_connect(GTK_BUTTON(pBouton[2]), "clicked", G_CALLBACK(), NULL);
+//		g_signal_connect(GTK_BUTTON(pBouton[3]), "clicked", G_CALLBACK(), NULL);
+//		g_signal_connect(GTK_BUTTON(pBouton[4]), "clicked", G_CALLBACK(), NULL);
+		g_signal_connect(GTK_BUTTON(pBouton[5]), "clicked", G_CALLBACK(go_menu_principal_user), NULL);
+	
+	
+		gtk_widget_show_all(pRecherche);
+	}
 }
+
 
--- proj_v_grph/gestion_graphique.h
+++ proj_v_grph/gestion_graphique.h
@@ -8,6 +8,9 @@ void menu_creation();
 void menu_acceuil();
 void menu_principal_user();
 void menu_principal_admin();
+void menu_recherche();
+
 void on_valider_connexion(GtkWidget *pButton, gpointer data);
+void on_valider_creation(GtkWidget *pButton, gpointer data);
 
 
