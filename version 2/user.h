4cb1c6beb09344497655d5e585331177eeacbbbc
Dyokiin <theo.arnauld-des-lions@univ-tlse3.fr>
2020-04-18 12:11:19 +0200

    menu chager mdp, non opé

---
+++ version 2/user.h
@@ -0,0 +1,26 @@
+/* user.h */
+#ifndef __USER_H__
+#define __USER_H__
+
+typedef struct s_user *User;
+
+
+
+int test_connexion(char *login, char *password);
+int test_creation_compte(char* login, char* password);
+
+int trouve_caractere_speciaux(char *chaine);
+
+int save_user(char *login,char *password);
+int chiffrement(char *mdp,char* password_chiffre);
+int ajout_ligne_utilisateur(User u);
+int recherche_occurence_login(char *login);
+int creation_fichier_historique_utilisateur(char *login);
+
+int recherche_login_renvoie_ligne(char *login, char *ligne_retournee);
+int transforme_ligne_user_en_sa_structure(User u);
+
+int supprimer_et_ajouter_ligne_utilisateur(User u);
+
+
+#endif
